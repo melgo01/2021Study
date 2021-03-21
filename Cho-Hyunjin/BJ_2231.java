8fdf97743cdd4d0ea44d3f700003166e667d652e
Hyunjin Cho <roycecho@gm.gist.ac.kr>
2021-03-21 22:17:17 +0900

    210321

--- Cho-Hyunjin/BJ_2231.java
+++ Cho-Hyunjin/BJ_2231.java
@@ -1,10 +1,32 @@
 package java_problems;
 
+import java.util.Scanner;
+
 public class BJ_2231 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		int result = 0;
+		
+		for(int i = 0; i < N; i++) {
+			int n = i;
+			int sum = 0;
+			
+			while(n != 0) {
+				sum += n % 10;
+				n /= 10;
+			}
+			
+			if(sum + i == N) {
+				result = i;
+				break;
+			}
+		}
+		
+		System.out.println(result);
 	}
 
 }
