8f989c51f89d2d7feaa5d8ffb16497672b82c904
abell2667 <79260453+abell2667@users.noreply.github.com>
2021-03-22 19:47:44 +0900

    Update Bj_10818.cpp

--- C++/Example5/Bj_10818.cpp
+++ C++/Example5/Bj_10818.cpp
@@ -4,7 +4,7 @@ int main() {
     int n;
     std::cin >> n;
     int arr[n];
-    std::fill_n(arr, n, 0);
+    std::fill_n(arr, n, 0); // arr이라는 길이가 n인 배열을 0으로 채우기.
     
     int max = -1000000;
     int min = 1000000;
@@ -19,3 +19,4 @@ int main() {
 
 }
 
+// 백준에서는 코드가 잘 돌아갔는데 이상하게 vscode에서는 실행이 안 됩니다.. 왠지 모르겠네요...
